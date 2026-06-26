b4b4397ebfbe43e5f3377f428654048f47ad180e
Aprim <aprimregmi24@gmail.com>
2026-06-26 16:13:59 +0545

    add: parser for prefix operators and documented the parser

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -128,3 +128,23 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+// PrefixExpression: e.g: - 5, - a.
+type PrefixExpression struct {
+	Token    lexer.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(" (")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(") ")
+
+	return out.String()
+}
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -41,6 +41,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[lexer.TokenType]prefixParseFn)
 	p.registerPrefix(lexer.IDENT, p.parseIdentifier)
 	p.registerPrefix(lexer.INT, p.parseIntegerLiteral)
+	p.registerPrefix(lexer.EXCLAM, p.parsePrefixExpression)
+	p.registerPrefix(lexer.MINUS, p.parsePrefixExpression)
 
 	// Read two tokens to initialize both curToken and peekToken
 	p.nextToken()
@@ -48,20 +50,32 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
+// Errors returns the list of errors found during parsing.
 func (p *Parser) Errors() []string {
 	return p.errors
 }
 
+// peekError push an error to the Errors slice.
+// Called if the next token type doesn't match the expected token type.
 func (p *Parser) peekError(t lexer.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %v, got %v instead", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
 
+// noPrefixParseFnError push an error to the Errors slice.
+// Called if an expression starts with an undefined token.
+func (p *Parser) noPrefixParseFnError(t lexer.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+// nextToken increments the curToken and peekToken pointer by one position.
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 
+// ParseProgram is the entry point. It loops through the entire token stream until EOF token to build the root AST Program node.
 func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
@@ -77,6 +91,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	return program
 }
 
+// parseStatement checks the current token and routes it to the respective statement parser
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case lexer.BAMBOO:
@@ -88,14 +103,18 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
+// parseIdentifier creates an AST node for a variable name or identifier.
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
+// parseExpression is the core Pratt parser engine. It parses expressions
+// based on operator priority (precedence).
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
@@ -103,6 +122,7 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	return leftExp
 }
 
+// parseBambooStatement parses a variable assignment line (e.g., 'bamboo x = 5;').
 func (p *Parser) parseBambooStatement() *ast.BambooStatement {
 	stmt := &ast.BambooStatement{Token: p.curToken}
 
@@ -125,6 +145,7 @@ func (p *Parser) parseBambooStatement() *ast.BambooStatement {
 	return stmt
 }
 
+// parseReturnStatement parses a return statement (e.g., 'return 5;').
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 	p.nextToken()
@@ -139,6 +160,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
+// parseExpressionStatement parses expression (e.g., '5 + 5;' or 'x;').
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 
@@ -151,6 +173,7 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+// parseIntegerLiteral converts the current token text into a valid number AST node.
 func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 
@@ -165,14 +188,35 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	return lit
 }
 
+// parsePrefixExpression handles '!' or '-' operators.
+// It saves the operator, moves after operator, and then parses whatever comes next.
+// e.g: -5
+// first it saves the operator '-' and moves after it and then parse the expression '5' in this case
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expr := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+	// Parse the expression after the operator
+	expr.Right = p.parseExpression(PREFIX)
+	return expr
+}
+
+// curTokenIs checks if the token parser is looking at matches a specific token type.
 func (p *Parser) curTokenIs(t lexer.TokenType) bool {
 	return p.curToken.Type == t
 }
 
+// peekTokenIs looks at the next token after curToken and checks if it matches a specific token type
 func (p *Parser) peekTokenIs(t lexer.TokenType) bool {
 	return p.peekToken.Type == t
 }
 
+// expectPeek checks if the next token is of the expected type.
+// If yes, it moves to next token and returns true.
+// If no, it logs syntax error and returns false.
 func (p *Parser) expectPeek(t lexer.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
