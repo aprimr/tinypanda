02dadb23a0d7bb71d79ca7b08e9306308802ddf9
Aprim <aprimregmi24@gmail.com>
2026-06-29 14:53:28 +0545

    add eval for return statements

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -17,7 +17,7 @@ func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -39,21 +39,44 @@ func Eval(node ast.Node) object.Object {
 		return evalInfixExpression(node.Operator, left, right)
 
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatements(node)
 
 	case *ast.IffExpression:
 		return evalIffExpression(node)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	}
 
 	return nil
 }
 
-// evalStatements iterates sequentially through a block of statements
-func evalStatements(stmts []ast.Statement) object.Object {
+// evalProgram iterates sequentially through a block of statements
+func evalProgram(stmts []ast.Statement) object.Object {
 	var result object.Object
 
 	for _, stmt := range stmts {
 		result = Eval(stmt)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+// parseBlockStatement parses  statements enclosed in curly braces { ... }.
+func evalBlockStatements(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OVJ {
+			return result
+		}
 	}
 
 	return result
